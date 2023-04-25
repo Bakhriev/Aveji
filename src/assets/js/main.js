9108b139009b2747beef651ff09361c272f06126
Bakhriev <legendofpain5112@gmail.com>
2023-04-25 13:29:05 +0500

    add src folder

---
+++ src/assets/js/main.js
@@ -0,0 +1,50 @@
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const navigation = document.querySelector('.header__navigation')
+  const overlay = document.querySelector('.overlay')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    navigation.classList.toggle('active')
+    overlay.classList.toggle('active')
+    document.body.classList.add('scroll-lock')
+  })
+
+  overlay.addEventListener('click', () => {
+    burger.classList.remove('active')
+    navigation.classList.remove('active')
+    overlay.classList.remove('active')
+    document.body.classList.remove('scroll-lock')
+  })
+
+  // window.addEventListener("resize", () => {
+  // 	if (window.innerWidth > 991.98) {
+  // 		burger.classList.remove("active")
+  // 		navigation.classList.remove("active")
+  // 		overlay.classList.remove("active")
+  // 	}
+  // })
+}
+burgerMenu()
+
+let options = {
+  threshold: 0.4,
+}
+
+const onVisible = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.querySelectorAll('.about__column').forEach((col) => {
+        col.classList.add('visible')
+      })
+    }
+  })
+}
+
+const aboutRow = document.querySelectorAll('.about__row')
+
+let observer = new IntersectionObserver(onVisible, options)
+
+aboutRow.forEach((col) => {
+  observer.observe(col)
+})
